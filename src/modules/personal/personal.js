4f81eb65d4b29e5b0daebbd80e4519e0c7237576
dwarjie <mozencode@gmail.com>
2022-02-09 13:59:43 +0800

    Finished: Added Active state to personal once project is clicked.

--- src/modules/personal/personal.js
+++ src/modules/personal/personal.js
@@ -1,5 +1,7 @@
 import { Render_Project_Adder } from '../renderer/render_project_adder.js';
 import { Render_Todo } from '../renderer/render.js';
+import { Render_Button_State } from '../renderer/render_button_state.js';
+import { Clear_Button_State } from '../renderer/clear_button_state.js';
 import { Cancel_Project } from './cancel_adding_project.js';
 import { Add_Project } from './add_project.js';
 import { Add_Todo } from '../add_todo/add_todo.js';
@@ -23,6 +25,8 @@ export const Personal = (function() {
 	// this function will handle all the rendering for the view-todo view 
 	// this will be only called by the use of side navigation project title
 	function render(e) {
+		Clear_Button_State();
+		Render_Button_State("#show-personal");
 		Clear_Content(); // clear all the content inside the main view todo
 		// get the target innerHTML and use it to change project title
 		projectTitle.innerHTML = e.target.innerHTML;
