f3eacc45e3266fcfc9591bb7e057e157006e9fb1
dwarjie <mozencode@gmail.com>
2022-02-22 23:38:36 +0800

    Fixed: Update Modal now closing when first clicked.

--- src/modules/renderer/render_update_modal.js
+++ src/modules/renderer/render_update_modal.js
@@ -1,4 +1,5 @@
 import { Modal } from '../top_navigation/show_modal.js';
+import { Windows_Click_Event } from '../event/window_click_event.js';
 // this render module will handle the rendering of modal for updating the todo item
 // this will reuse the show-modal class element for adding todo
 // but will change the innerHTML of the button from "Add" to "Update"
@@ -7,4 +8,5 @@ const updateButton = document.querySelector('#add-item');
 export function Render_Update_Modal() {
 	Modal.show("show-modal");
 	updateButton.innerHTML = "Update";
+	window.addEventListener('click', Windows_Click_Event);
 }
